f33473a292ceea46dd788b9340c377f06d99d15d
Sean Morris <sean@seanmorr.is>
2021-06-07 23:33:22 -0400

    Debuffer

--- source/HomeRoute.php
+++ source/HomeRoute.php
@@ -118,7 +118,7 @@ class HomeRoute implements \SeanMorris\Ids\Routable
 		$request = $router->request();
 
 		header('HTTP/1.1 200 OK');
-		header('Transfer-Encoding: chunked');
+		// header('Transfer-Encoding: chunked');
 		header('Content-Type: text/event-stream');
 		header('Cache-Control: no-cache');
 		header('Connection: keep-alive');
